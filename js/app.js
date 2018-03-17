6f7cf51bee7f273f0a59eb882e0121f4505d29ac
zelia <zelia@o2.pl>
2018-03-17 16:29:53 +0100

    better

--- js/app.js
+++ js/app.js
@@ -17,30 +17,24 @@ document.addEventListener('DOMContentLoaded', function() {
    
 
 
+  var buttons = document.querySelectorAll('.read-more');
 
-var buttons = document.querySelectorAll('.read-more');
-    console.log(buttons);
+  function showHide() {
 
+    var textArea = this.previousElementSibling;
+   
+    if (textArea.style.display === 'none' || textArea.style.display === '') {
+      textArea.style.display = 'block';
+      this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+    } else {
+      textArea.style.display = 'none';
+      this.innerHTML = 'WIECEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+    }
+  }
 
-
-    
-       buttons[0].addEventListener('click', function() {
-var textArea = this.previousElementSibling;
-      
-      if (textArea.style.display === 'none' || textArea.style.display === '') {
-
-
-        textArea.style.display = 'block';
-        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
-      } else {
-
-       
-        textArea.style.display = 'none';
-        this.innerHTML = 'WIECEJ <span class="glyphicon glyphicon-chevron-down"></span>';
-      }
-
-    });
-    
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', showHide);
+  }
     
     
 
